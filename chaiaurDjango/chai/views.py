965e0a919c1968d4621282dcb9372416fdffb823
PARAM MITTAL <parammittal16@gmail.com>
2025-07-30 00:15:51 +0530

    Add files via upload

---
+++ chaiaurDjango/chai/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, get_object_or_404
+from .models import ChaiVarity
+
+# Create your views here.
+def all_chai(request):
+    chais = ChaiVarity.objects.all()
+    return render(request, 'chai/all_chai.html', { 'chais': chais })
+
+def chai_detail(request, chai_id):
+    chai = get_object_or_404(ChaiVarity, pk=chai_id)
+    return render(request, 'chai/chai_detail.html', { 'chai': chai })
